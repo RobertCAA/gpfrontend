8511e3dfc54f962995e0a01be83fe900b8e8087b
RobertCAA <rober.ce.az@gmail.com>
2022-10-15 20:21:24 +0200

    Created UserList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
+import UserList from "./api/users/UserList";
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Public />} />
+        <Route path='users'>
+          <Route index element={<UserList />} />
+        </Route>
       </Route>
     </Routes>
   );
